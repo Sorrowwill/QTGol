23fc07cf0d83c575c42c33ea9fedf5e4c05539d3
Sorrowwill <ssorrowwill@gmail.com>
2022-08-16 21:25:38 +0300

    working hello world app

--- tests/test_qtgol.py
+++ tests/test_qtgol.py
@@ -1,5 +1,17 @@
 from qtgol import __version__
+from pytestqt.qtbot import QtBot
+from qtgol.app import MyWidget
+from PySide6 import QtCore
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_hello(qtbot: QtBot):
+    widget = MyWidget()
+    qtbot.addWidget(widget)
+
+    qtbot.mouseClick(widget.button, QtCore.Qt.LeftButton)
+
+    assert widget.text.text() in ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
